d9fdaf2b009df9596a067cefa61bfa5f3440ee5d
Aniket Sarbha <sarbhaaniket@gmail.com>
2025-03-06 02:44:24 +0530

    updated

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"siJxsQSZ874GyEBbNMjGwEPUch1/pXLEwitZxL+K074=\"\n}"
--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://poetic-salmon-66.clerk.accounts.dev",
+      domain: "https://related-newt-91.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
